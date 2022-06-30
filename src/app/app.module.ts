53fb5a328129eddde3d7ef9603f4cf8c84f54ac7
Victoria Derudder <viki.derudder@gmail.com>
2022-06-30 14:44:59 -0300

    feat: added base components
    Components:
    -Home page
    -Coins list
    -Coin detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoinsListComponent } from './coins-list/coins-list.component';
+import { CoinDetailComponent } from './coin-detail/coin-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoinsListComponent,
+    CoinDetailComponent
   ],
   imports: [
     BrowserModule,
