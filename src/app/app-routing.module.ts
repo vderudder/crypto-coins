7ff145003a0dce8edaaa9cd2adb6ad7c35a0bf65
Victoria Derudder <viki.derudder@gmail.com>
2022-07-01 14:34:37 -0300

    fix: redirect to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'list', component: CoinsListComponent},
   {path: 'coin/:id', component: CoinDetailComponent},
+  {path: '**', redirectTo: '/'},
 
 ];
 
